64fa8d54a986ef4002394299f6934f920c9bde1f
yuwenyong <yuwenyong@localhost.localdomain>
2017-09-19 10:13:00 +0800

    add httputils

---
+++ src/net4cxx/common/httputils/cookie.h
@@ -0,0 +1,188 @@
+//
+// Created by yuwenyong on 17-9-19.
+//
+
+#ifndef NET4CXX_COMMON_HTTPUTILS_COOKIE_H
+#define NET4CXX_COMMON_HTTPUTILS_COOKIE_H
+
+#include "net4cxx/common/common.h"
+#include <boost/algorithm/string.hpp>
+#include <boost/regex.hpp>
+#include "net4cxx/common/utilities/errors.h"
+
+NS_BEGIN
+
+NET4CXX_DECLARE_EXCEPTION(CookieError, Exception);
+
+
+class NET4CXX_COMMON_API CookieUtil {
+public:
+    static std::string quote(const std::string &s, const std::vector<char> &legalChars= CookieUtil::legalChars,
+                             const std::array<char, 256> &idmap= CookieUtil::idmap);
+
+    static std::string unquote(const std::string &s);
+
+    static const std::vector<char> legalChars;
+    static const std::array<char, 256> idmap;
+    static const std::map<char, std::string> translator;
+    static const boost::regex octalPatt;
+    static const boost::regex quotePatt;
+    static const char *legalCharsPatt;
+    static const boost::regex cookiePattern;
+};
+
+
+class NET4CXX_COMMON_API Morsel {
+public:
+    Morsel();
+
+    std::string& operator[](const std::string &key);
+
+    bool isReservedKey(const std::string &key) const {
+        return reserved.find(boost::to_lower_copy(key)) != reserved.end();
+    }
+
+    const std::string& getValue() const {
+        return _value;
+    }
+
+    void set(const std::string &key, const std::string &val, const std::string &codedVal,
+             const std::vector<char> &legalChars= CookieUtil::legalChars,
+             const std::array<char, 256> &idmap= CookieUtil::idmap);
+
+    std::string output(const StringMap *attrs= nullptr, const std::string &header= "Set-Cookie:") const {
+        return header + " " + outputString(attrs);
+    }
+
+    std::string outputString(const StringMap *attrs= nullptr) const;
+
+    static const StringMap reserved;
+protected:
+    StringMap _items;
+    std::string _key;
+    std::string _value;
+    std::string _codedValue;
+};
+
+
+class NET4CXX_COMMON_API BaseCookie {
+public:
+    typedef std::tuple<std::string, std::string> DecodeResultType;
+    typedef std::tuple<std::string, std::string> EncodeResultType;
+    typedef std::map<std::string, Morsel> MorselContainerType;
+    typedef std::function<void (const std::string&, const Morsel&)> CallbackType;
+
+    class CookieSetter {
+    public:
+        CookieSetter(BaseCookie *cookie, const std::string *key, Morsel *morsel)
+                : _cookie(cookie)
+                , _key(key)
+                , _morsel(morsel) {
+
+        }
+
+        CookieSetter& operator=(const std::string &value) {
+            std::string rval, cval;
+            std::tie(rval, cval) = _cookie->valueEncode(value);
+            _morsel->set(*_key, rval, cval);
+            return *this;
+        }
+    protected:
+        BaseCookie *_cookie;
+        const std::string *_key;
+        Morsel *_morsel;
+    };
+
+    BaseCookie() = default;
+
+    explicit BaseCookie(const std::string &input) {
+        load(input);
+    }
+
+    explicit BaseCookie(const StringMap &input) {
+        load(input);
+    }
+
+    virtual ~BaseCookie() = default;
+
+    const Morsel& at(const std::string &key) const {
+        return _items.at(key);
+    }
+
+    Morsel& at(const std::string &key) {
+        return _items.at(key);
+    }
+
+    CookieSetter operator[](const std::string &key) {
+        auto iter = _items.find(key);
+        if (iter == _items.end()) {
+            auto result = _items.emplace(key, Morsel());
+            iter = result.first;
+        }
+        return {this, &(iter->first), &(iter->second)};
+    }
+
+    bool has(const std::string &key) const {
+        return _items.find(key) != _items.end();
+    }
+
+    void erase(const std::string &key) {
+        _items.erase(key);
+    }
+
+    void getAll(const CallbackType &callback) const {
+        for (auto &item: _items) {
+            callback(item.first, item.second);
+        }
+    }
+
+    virtual DecodeResultType valueDecode(const std::string &val);
+
+    virtual EncodeResultType valueEncode(const std::string &val);
+
+    std::string output(const StringMap *attrs= nullptr, const std::string &header= "Set-Cookie:",
+                       const std::string &sep= "\015\012") const;
+
+    void load(const std::string &rawdata) {
+        parseString(rawdata);
+    }
+
+    void load(const StringMap &rawdata) {
+        for (auto &kv: rawdata) {
+            (*this)[kv.first] = kv.second;
+        }
+    }
+
+    void clear() {
+        _items.clear();
+    }
+protected:
+    void set(const std::string &key, const std::string &realValue, const std::string &codedValue) {
+        auto iter = _items.find(key);
+        if (iter == _items.end()) {
+            auto result = _items.emplace(key, Morsel());
+            iter = result.first;
+        }
+        iter->second.set(key, realValue, codedValue);
+    }
+
+    void parseString(const std::string &str, const boost::regex &patt =CookieUtil::cookiePattern);
+
+    MorselContainerType _items;
+};
+
+
+class NET4CXX_COMMON_API SimpleCookie: public BaseCookie {
+public:
+    using BaseCookie::BaseCookie;
+
+    SimpleCookie() = default;
+
+    DecodeResultType valueDecode(const std::string &val) override;
+
+    EncodeResultType valueEncode(const std::string &val) override;
+};
+
+NS_END
+
+#endif //NET4CXX_COMMON_HTTPUTILS_COOKIE_H
---
+++ src/net4cxx/common/httputils/urlparse.h
@@ -0,0 +1,186 @@
+//
+// Created by yuwenyong on 17-9-19.
+//
+
+#ifndef NET4CXX_COMMON_HTTPUTILS_URLPARSE_H
+#define NET4CXX_COMMON_HTTPUTILS_URLPARSE_H
+
+#include "net4cxx/common/common.h"
+#include <tuple>
+#include <boost/optional.hpp>
+
+NS_BEGIN
+
+using URLSplitResultBase = std::tuple<std::string, std::string, std::string, std::string, std::string>;
+
+
+class NET4CXX_COMMON_API URLSplitResult: public URLSplitResultBase {
+public:
+    URLSplitResult()
+            : URLSplitResultBase() {
+
+    }
+
+    URLSplitResult(std::string scheme,
+                   std::string netloc,
+                   std::string path,
+                   std::string query,
+                   std::string fragment)
+            : URLSplitResultBase(std::move(scheme),
+                                 std::move(netloc),
+                                 std::move(path),
+                                 std::move(query),
+                                 std::move(fragment)) {
+
+    }
+
+    const std::string& getScheme() const {
+        return std::get<0>(*this);
+    }
+
+    const std::string& getNetloc() const {
+        return std::get<1>(*this);
+    }
+
+    const std::string& getPath() const {
+        return std::get<2>(*this);
+    }
+
+    const std::string& getQuery() const {
+        return std::get<3>(*this);
+    }
+
+    const std::string& getFragment() const {
+        return std::get<4>(*this);
+    }
+
+    boost::optional<std::string> getUserName() const;
+
+    boost::optional<std::string> getPassword() const;
+
+    boost::optional<std::string> getHostName() const;
+
+    boost::optional<unsigned short> getPort() const;
+
+    std::string getURL() const;
+};
+
+
+using URLParseResultBase = std::tuple<std::string, std::string, std::string, std::string, std::string, std::string>;
+
+
+class NET4CXX_COMMON_API URLParseResult : public URLParseResultBase {
+public:
+    URLParseResult()
+            : URLParseResultBase() {
+
+    }
+
+    URLParseResult(std::string scheme,
+                   std::string netloc,
+                   std::string path,
+                   std::string params,
+                   std::string query,
+                   std::string fragment)
+            : URLParseResultBase(std::move(scheme),
+                                 std::move(netloc),
+                                 std::move(path),
+                                 std::move(params),
+                                 std::move(query),
+                                 std::move(fragment)) {
+
+    }
+
+    const std::string& getScheme() const {
+        return std::get<0>(*this);
+    }
+
+    const std::string& getNetloc() const {
+        return std::get<1>(*this);
+    }
+
+    const std::string& getPath() const {
+        return std::get<2>(*this);
+    }
+
+    const std::string& getParams() const {
+        return std::get<3>(*this);
+    }
+
+    const std::string& getQuery() const {
+        return std::get<4>(*this);
+    }
+
+    const std::string& getFragment() const {
+        return std::get<5>(*this);
+    }
+
+    boost::optional<std::string> getUserName() const;
+
+    boost::optional<std::string> getPassword() const;
+
+    boost::optional<std::string> getHostName() const;
+
+    boost::optional<unsigned short> getPort() const;
+
+    std::string getURL() const;
+};
+
+
+using QueryArgMap = std::map<std::string, std::string>;
+using QueryArgMultiMap = std::multimap<std::string, std::string>;
+using QueryArgList = std::vector<std::pair<std::string, std::string>>;
+using QueryArgListMap = std::map<std::string, std::vector<std::string>>;
+
+
+class NET4CXX_COMMON_API URLParse {
+public:
+    static URLParseResult urlParse(std::string url, std::string cheme="", bool allowFragments=true);
+
+    static URLSplitResult urlSplit(std::string url, std::string scheme="", bool allowFragments=true);
+
+    static std::string urlUnparse(const URLParseResult &data);
+
+    static std::string urlUnsplit(const URLSplitResult &data);
+
+    static std::string urlJoin(const std::string &base, const std::string &url, bool allowFragments=true);
+
+    static std::tuple<std::string, std::string> urlDefrag(const std::string &url);
+
+    static std::string unquote(const std::string &s);
+
+    static std::string unquotePlus(const std::string &s);
+
+    static std::string quote(const std::string &s, std::string safe="/");
+
+    static std::string quotePlus(const std::string &s, const std::string &safe="");
+
+    static std::string urlEncode(const QueryArgMap &query);
+
+    static std::string urlEncode(const QueryArgMultiMap &query);
+
+    static std::string urlEncode(const QueryArgList &query);
+
+    static std::string urlEncode(const QueryArgListMap &query);
+
+    static QueryArgListMap parseQS(const std::string &queryString, bool keepBlankValues=false,
+                                    bool strictParsing=false);
+
+    static QueryArgList parseQSL(const std::string &queryString, bool keepBlankValues=false, bool strictParsing=false);
+protected:
+    static std::tuple<std::string, std::string> splitParams(const std::string &url);
+
+    static std::tuple<std::string, std::string> splitNetloc(const std::string &url, size_t start=0);
+
+    static const StringSet _usesRelative;
+    static const StringSet _usesNetloc;
+    static const StringSet _usesParams;
+    static const char * _schemeChars;
+    static const std::map<std::string, char> _hexToChar;
+    static const char * _alwaysSafe;
+    static const std::map<char, std::string> _safeMap;
+};
+
+NS_END
+
+#endif //NET4CXX_COMMON_HTTPUTILS_URLPARSE_H
